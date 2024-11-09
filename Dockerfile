93370f1f8690c123d4144bdfcf865f747452805d
pinhead <pinhead@pinhead.dev>
2024-11-08 18:40:29 -0600

    finished putting data in the template pages, includes basic redesign

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM Python
+COPY ./templates to_docker/templates
+COPY ./static to_docker/static
+COPY requirements.txt to_docker/requirements.txt
+COPY main.py to_docker/main.py
 RUN pip install -r ./requirements.txt
-COPY ./templates /home/itineraryplanner/templates
-COPY ./static /home/itineraryplanner/templates
-COPY main.py /home/itineraryplanner/main.py
 EXPOSE 8080
 CMD main.py
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import traceback
 from os import getenv
 from urllib import request
 
@@ -25,7 +26,8 @@ class UnknownError(Exception):
 async def get_weather(client_session: aiohttp.ClientSession, location: str):
     async with client_session.get('https://api.weatherapi.com/v1/forecast.json',
                                   params={"key": weather_key,
-                                          "q": location
+                                          "q": location,
+                                          "days": 1
                                           }) as response:
         if response.status != 200:
             error = await response.json()
@@ -36,7 +38,42 @@ async def get_weather(client_session: aiohttp.ClientSession, location: str):
                 f"  |->  Status: {response.status}\n"
                 f"  |->  Headers: {response.headers}\n"
                 f"  |->  Response: {await response.json()}\n")
-        return await response.json()
+        weather = await response.json()
+        rain_chance = weather["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"]
+        snow_chance = weather["forecast"]["forecastday"][0]["day"]["daily_chance_of_snow"]
+        direction = weather["current"]["wind_degree"]
+
+        precipitation = {
+            "chance": rain_chance,
+            "type": "Rain"
+        }
+
+        if snow_chance > rain_chance:
+            precipitation["chance"] = snow_chance
+            precipitation["type"] = "Snow"
+
+        if 337 < direction < 22:
+            weather["current"]["wind_arrow"] = "↑"
+        elif 292 < direction <= 337:
+            weather["current"]["wind_arrow"] = "↖"
+        elif 247 < direction <= 292:
+            weather["current"]["wind_arrow"] = "←"
+        elif 202 < direction <= 247:
+            weather["current"]["wind_arrow"] = "↙"
+        elif 157 < direction <= 202:
+            weather["current"]["wind_arrow"] = "↓"
+        elif 112 < direction <= 157:
+            weather["current"]["wind_arrow"] = "↘"
+        elif 67 < direction <= 112:
+            weather["current"]["wind_arrow"] = "→"
+        else:
+            weather["current"]["wind_arrow"] = "↗"
+
+        # fixes/additions
+        weather["current"]["condition"]["icon"] = f'https:{weather["current"]["condition"]["icon"]}'  # fix up image url
+        weather["current"]["precipitation"] = precipitation
+        latitude, longitude = weather["location"]["lat"], weather["location"]["lon"]
+        return weather, latitude, longitude
 
 
 async def get_places(client_session: aiohttp.ClientSession, latitude, longitude, radius: 5000, search_type: str):
@@ -54,12 +91,30 @@ async def get_places(client_session: aiohttp.ClientSession, latitude, longitude,
             address = place["vicinity"]
             rating = place.get("rating")
             total_ratings = place.get("user_ratings_total", 0)
-            price_level = place.get("price_level", "N/A")
+            price_level = place.get("price_level")
+
+            match price_level:
+                case 0:
+                    price_level = "Free"
+                case 1:
+                    price_level = "Inexpensive"
+                case 2:
+                    price_level = "Moderate"
+                case 3:
+                    price_level = "Expensive"
+                case 4:
+                    price_level = "Very Expensive"
+                case _:
+                    price_level = "N/A"
+
             try:
-                is_open = place["opening_hours"]["open_now"]
+                if place["opening_hours"]["open_now"]:
+                    is_open = "Currently Open"
+                else:
+                    is_open = "Closed"
             except Exception as error:
                 is_open = "N/A"
-            status = place["business_status"]
+            status = place["business_status"].title().replace("_", " ")
             types = place["types"]
             link = f'https://www.google.com/maps/place/?q=place_id:{place["place_id"]}'
 
@@ -85,14 +140,16 @@ async def itinerary():
 
         try:
             async with aiohttp.ClientSession() as client_session:
-                weather = await get_weather(client_session, location)
-                latitude, longitude = weather["location"]["lat"], weather["location"]["lon"]
+                weather, latitude, longitude = await get_weather(client_session, location)
                 attractions = await get_places(client_session, latitude, longitude, radius, 'tourist_attraction')
                 restaurants = await get_places(client_session, latitude, longitude, radius, 'restaurant')
-            return await render_template("/itinerary/index.html", weather=weather, attractions=attractions,
-                                         restaurants=restaurants)
+            return await render_template("/itinerary/index.html", location=weather["location"], weather=weather,
+                                         attractions=attractions, restaurants=restaurants)
         except Exception as error:
-            session['error'] = f"{error.__class__.__name__}: {str(error)}"
+            # raise error  # used for when something is wrong
+            session['error'] = '\n'.join([
+                ''.join(traceback.format_exception_only(None, error)).strip(),
+                ''.join(traceback.format_exception(None, error, error.__traceback__)).strip()])
             return redirect(url_for('index'))
     else:
         return redirect(url_for("index"))
